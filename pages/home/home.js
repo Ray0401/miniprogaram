beb9c7caa54ce7ac57ee3f845b568e7693d1e6bc
徐海瑞 <cgxuhairui@163.com>
2019-12-12 21:51:03 +0800

    分享功能

--- pages/home/home.js
+++ pages/home/home.js
@@ -37,52 +37,29 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
+  clickBtn() {
+    // wx.showActionSheet({
+    //   itemList: ['A', 'B', 'C'],
+    //   success(res) {
+    //     console.log(res.tapIndex)
+    //   },
+    //   fail(res) {
+    //     console.log(res.errMsg)
+    //   }
+    // })
+    wx.showLoading({
+      title: '成功',
+      icon: 'success',
+      duration: 2000,
 
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+  // 分享
+  onShareAppMessage:function(options){
+    return {
+      title:'我是转发',
+      path:'/pages/login/login',
+      imageUrl:'' //想要替换的图片地址
+    }
   }
 })
